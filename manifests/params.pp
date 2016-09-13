a6276262ca023b406f2ef098aef236d559b8603e
Jordia Garcia <jordi@improbable.io>
2016-09-13 14:33:17 +0100

    test

--- manifests/params.pp
+++ manifests/params.pp
@@ -63,6 +63,4 @@ class teamcity::params {
   $teamcity_agent_mem_opts = '-Xms512m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8'
   $custom_properties       = { }
   $launcher_wrapper_conf   = { }
-
-  fail("TEST '${launcher_wrapper_conf}'")
 }
